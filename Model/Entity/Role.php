46e06a79f09e2d93e0bbae138e574d5b94165719
Jan Macháček <jan.machacek@mall.cz>
2014-12-08 17:21:57 +0100

    Gettery a settery

--- Model/Entity/Role.php
+++ Model/Entity/Role.php
@@ -10,20 +10,54 @@ use Doctrine\Common\Collections\ArrayCollection;
  */
 class Role
 {
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer")
      */
     protected $id;
-    
+
     /**
      * @ORM\OneToMany(targetEntity="User", mappedBy="role")
      */
     protected $users;
-    
-    public function __construct(){
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+
+    public function __construct()
+    {
         $this->users = new ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
+    public function getID()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * 
+     * @param string $name
+     * @return \Model\Entity\Role
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
     }
 }
 
